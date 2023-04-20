89c90f97aa28eef3cc4fbce96938930c10f5851e
DerRiese <DerRiese765@gmail.com>
2023-04-20 15:38:06 +0300

    -

--- main.js
+++ main.js
@@ -14,7 +14,7 @@ const isInt = Number.isInteger(str)
 console.log(isInt);
 */
 
-function square(number){
+/*function square(number){
     if(isNaN(number))
     return 'Invalid input'
     return number * number
@@ -26,3 +26,39 @@ console.log(randomNumber);
 
 let int = 12;
 console.log(int.toString(2));
+*/
+//call back:
+/*
+function sum(firstNumber, secondNumber){
+    return firstNumber + secondNumber
+}
+function sub(firstNumber, secondNumber){
+    return firstNumber - secondNumber
+}
+
+function mul(firstNumber, secondNumber){
+    return firstNumber * secondNumber
+
+}
+
+function operation(firstNumber, secondNumber, opCallback){
+    return `[${opCallback(firstNumber, secondNumber)}]`
+
+}
+console.log(operation(5,2, sum));
+console.log(operation(5,2, sub));
+console.log(operation(5,2, mul));
+*/
+//for each
+const colors = ['red', 'green', 'blue']
+
+const isContain = colors.includes('red')
+console.log(isContain);
+
+
+/*const ages = [11, 13, 26, 15, 12]
+const hasAdult = ages.every(function(element){
+    return element >= 18
+})
+console.log(hasAdult);
+*/

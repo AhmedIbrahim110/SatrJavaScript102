8bff1fa78646fe77e77d3579b244354079f0c2ad
DerRiese <DerRiese765@gmail.com>
2023-04-24 12:41:25 +0300

    -

--- project.js
+++ project.js
@@ -1,5 +1,56 @@
 class vehicle{
-    constructor(name, companyName, ID);
-    
-
+    constructor(name, companyName, ID){
+        this.name = name
+        this.companyName = companyName
+        this.ID = ID
+    }
 }
+
+class car extends vehicle{
+    constructor(name, companyName, ID, CarType){
+        super(name, companyName, ID);
+        this.CarType = CarType;
+}
+}
+class plane extends vehicle{
+    constructor(name, companyName, ID, PlaneType){
+        super(name, companyName, ID);
+        this.PlaneType = PlaneType;
+
+    }
+}
+
+
+class employee{
+    constructor(Ename, EID, DoB){
+        this.Ename = Ename
+        this.EID = EID
+        this.DoB = DoB
+    }
+}
+
+class driver extends employee{
+
+    }
+
+
+class pilot extends employee{
+
+    }
+
+
+class reservation{
+    constructor(reservationID, vehiclesID, EmployeeID, reservationDate){
+        this.reservationID = reservationID
+        this.vehiclesID = vehiclesID
+        this.EmployeeID = EmployeeID
+        this.reservationDate = reservationDate
+    }
+}
+//cars
+const Emgrand = new car("Emgrand", "Geely", "1", "gas")
+const Camry = new car("Camry", "Toyota", "2", "gas")
+const Elantra = new car("Elantra", "Hyundai", "3", "gas")
+//planes
+const AC130 = new plane("AC-130", "ward", "01", "jet")
+const VTOL = new plane ("VTOL", "ward", "02", "jet")
